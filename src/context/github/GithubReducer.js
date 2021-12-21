24db9dd97636b035ade01f13dcdeb434d3ae997b
Jannik Oeschger <oeschgerjannik@gmail.com>
2021-12-21 21:13:38 +0100

    ♻️ Cleaned up ActionsFile & refactored to axios
    Cleaned up ActionsFile by using axios instead of fetch and by merging userData and userRepos functions into one function

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -6,12 +6,6 @@ const githubReducer = (state, action) => {
                 users: action.payload,
                 isLoading: false,
             }
-        case 'GET_USER':
-            return {
-                ...state,
-                user: action.payload,
-                isLoading: false
-            }
         case 'SET_LOADING':
             return {
                 ...state,
@@ -22,10 +16,11 @@ const githubReducer = (state, action) => {
                 ...state,
                 users: []
             }
-        case 'GET_REPOS':
+        case 'GET_USER_AND_REPOS':
             return {
                 ...state,
-                repos: action.payload,
+                repos: action.payload.repos,
+                user: action.payload.user,
                 isLoading: false
             }
         default:
